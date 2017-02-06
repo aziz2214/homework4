9af1163408eb4dc00fdd775c91ff2d9cc655973b
aziz2214 <ra2296@nyu.edu>
2017-02-06 18:17:25 -0500

    complete commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express'); 
+var app = express();
+var path =require("path"); 
+
+
+
+app.get('/', function(req, res) { 
+    res.sendFile(path.join(__dirname+'/pages/home.html'));
+})
+
+app.get('/start', function(req, res) { 
+    res.sendFile(path.join(__dirname+'/pages/start.html'));
+        
+})
+
+app.get('/ironman', function(req, res) { 
+    res.sendFile(path.join(__dirname+'/pages/spiderman.html'));
+        
+})
+
+app.get('/spiderman', function(req, res) { 
+    res.sendFile(path.join(__dirname+'/pages/captainamerica.html'));
+        
+})
+
+app.get('/captainamerica', function(req, res) { 
+    res.sendFile(path.join(__dirname+'/pages/thor.html'));
+        
+})
+
+app.get('/thor', function(req, res) { 
+    res.sendFile(path.join(__dirname+'/pages/end.html'));
+        
+})
+
+
+
+
+app.listen(3000, function() { 
+    console.log("App Started Successfully!")
+})
+
